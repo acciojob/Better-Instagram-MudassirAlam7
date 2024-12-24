135db3c0877417aef11571d4f5d4f671f06a65b0
MudassirAlam7 <mudassiralam7@gmail.com>
2024-12-25 00:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let imageList = document.getElementsByClassName('image')
+let firstElement = null
+for(let list of imageList){
+	list.addEventListener('dragstart', function (e) {
+		firstElement = e.target
+	})
+	list.addEventListener('dragover', function (e) {
+		e.preventDefault()	
+	})
+	list.addEventListener('drop', function(e){
+		e.preventDefault()
+		let secondElement = e.target
+		if(firstElement && firstElement!==secondElement){
+			let firstBg = window.getComputedStyle(firstElement).backgroundImage
+			let secondBg = window.getComputedStyle(secondElement).backgroundImage
+
+			firstElement.style.backgroundImage = secondBg
+			secondElement.style.backgroundImage = firstBg
+		}
+		firstElement = null
+	})
+}
